1b84b1993b3a78f070dc0e599565aa38ca0f5fb3
Eduardo González <egonzalez.garcia@atsistemas.com>
2021-01-22 22:45:24 +0100

    Initial Commit

---
+++ WeatherAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppDemo
+//
+//  Created by Eduardo González on 22/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
