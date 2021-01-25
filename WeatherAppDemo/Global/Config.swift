d767131320576b87b16d7b6ccea8ac84ed9ff36e
Eduardo González <egonzalez.garcia@atsistemas.com>
2021-01-25 19:28:45 +0100

    Added xcconfig files for environment configuration

--- WeatherAppDemo/Global/Config.swift
+++ WeatherAppDemo/Global/Config.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 struct Config {
-    static let appId = "61fa5c0663e197ff714324c1f6c0728d"
+    static var appId: String {
+        return Bundle.main.infoDictionary!["OPEN_WEATHER_MAP_APPID"] as? String ?? ""
+    }
     static let demoCoordinates = Coordinates(longitud: 11.57549, latitud: 48.13743)
     static let demoCityName = "Munich"
 }
